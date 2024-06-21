e7f69ba663f98b71fcf210fb9668a8ad84585537
CHIA HONG CHANG <vic147569az@gmail.com>
2024-06-21 20:35:15 +0800

    feat(k6test): add k6 test

---
+++ test.js
@@ -0,0 +1,27 @@
+import http from 'k6/http'
+import { check } from 'k6'
+
+export const options = {
+  duration: '30s',
+  vus: 10
+}
+
+export default function () {
+  const url = 'http://localhost:8081/request-registration'
+
+  const payload = JSON.stringify({
+    email: 'test@example.com'
+  })
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const res = http.post(url, payload, params)
+
+  check(res, {
+    isStatus204: (res) => res.status === 204
+  })
+}
